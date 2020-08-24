9406476f23c563c91f7a8de694e3b2d702a8750b
matsui <matsui@posl.ait.kyushu-u.ac.jp>
2020-08-24 22:02:42 +0900

    add some samples

---
+++ generic_sample/src/main.rs
@@ -0,0 +1,67 @@
+extern crate generic_sample;
+use generic_sample::Tweet;
+use generic_sample::NewsArticle;
+use generic_sample::Summary;
+
+use std::fmt::Display;
+
+fn main() {
+    let number_list = vec![34, 50, 25, 100, 65];
+    let result = largest(&number_list);
+    println!("The largest number is {}", result);
+
+    let char_list = vec!['y', 'm', 'a', 'q'];
+    let result = largest(&char_list);
+    println!("The largest char is {}", result);
+
+    let tweet = Tweet {
+        username: String::from("horese_ebooks"),
+        content: String::from("of course, as you probably already know, people."),
+        reply: false,
+        retweet: false,
+    };
+
+    println!("1 new tweet: {}", tweet.summarize());
+
+    let article = NewsArticle {
+        headline: String::from("Penguins win the Stanley Cup Championship!"),
+        location: String::from("Pittsburgh, PA, USA"),
+        author: String::from("Iceburgh"),
+        content: String::from("The Pittsburgh Penguins once again are the best hoceky team in th NHL."),
+    };
+
+    println!("New article available! {}", article.summarize());
+}
+
+fn largest<T: PartialOrd + Copy>(list: &[T]) -> T {
+    let mut largest = list[0];
+
+    for &item in list.iter() {
+        if item > largest {
+            largest = item;
+        }
+    }
+
+    largest
+}
+
+struct Pair<T> {
+    x: T,
+    y: T,
+}
+
+impl<T> Pair<T> {
+    fn new(x: T, y: T) -> Self {
+        Self { x, y }
+    }
+}
+
+impl<T: Display + PartialOrd> Pair<T> {
+    fn cmp_display(&self) {
+        if self.x >= self.y {
+            println!("The largest member is x = {}", self.x);
+        } else {
+            println!("The largest member is y = {}", self.y);
+        }
+    }
+}
